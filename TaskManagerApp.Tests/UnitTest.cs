422d0324f0b26f552cf9582454f9a8e85b912f7a
unknown <vikadav64@gmail.com>
2025-10-30 15:46:03 +0200

    TaskItemTests added

--- TaskManagerApp.Tests/UnitTest.cs
+++ TaskManagerApp.Tests/UnitTest.cs
@@ -5,19 +5,64 @@ namespace TaskManagerApp.Tests
 {
     [TestFixture]
     public class TaskItemTests
-    {   
+    {
         private TaskItem _task;
+
         [SetUp]
         public void Setup()
         {
             _task = new TaskItem("Test Task", "Test Description");
         }
 
-        [Test] // Метод тесту
+        [Test]
+        public void TaskItem_ShouldInitializeTitleAndDescription()
+        {
+            // Arrange + Act handled in SetUp
+            // Assert
+            Assert.That(_task.Title, Is.EqualTo("Test Task"));
+            Assert.That(_task.Description, Is.EqualTo("Test Description"));
+        }
+
+        [Test]
+        public void TaskItem_ShouldInitializeTaskAndSetStateToPending()
+        {
+            // Arrange + Act handled in SetUp
+            // Assert
+            Assert.That(_task.State, Is.EqualTo(TaskState.Pending));
+        }
+
+        [Test]
         public void MarkCompleted_ShouldSetStateToCompleted()
         {
-            _task.MarkCompleted(); 
+            _task.MarkCompleted();
             Assert.That(_task.State, Is.EqualTo(TaskState.Completed));
+        }
+
+        [Test]
+        public void Start_ShouldSetStateToInProgress()
+        {
+            _task.Start();
+            Assert.That(_task.State, Is.EqualTo(TaskState.InProgress));
+        }
+
+        [Test]
+        public void Reset_ShouldSetStateToPending_WhenTaskWasCompleted()
+        {
+            _task.MarkCompleted();
+
+            _task.Reset();
+
+            Assert.That(_task.State, Is.EqualTo(TaskState.Pending));
+        }
+
+        [Test]
+        public void Reset_ShouldSetStateToPending_WhenTaskWasInProgress()
+        {
+            _task.Start();
+
+            _task.Reset();
+
+            Assert.That(_task.State, Is.EqualTo(TaskState.Pending));
         }
     }
 
--- TaskManagerApp.Tests/obj/Debug/net8.0/TaskManagerApp.Tests.AssemblyInfo.cs
+++ TaskManagerApp.Tests/obj/Debug/net8.0/TaskManagerApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14c014eff84269ec3a5cdea217e7a01fb09235b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba33bc5e6f0a71f33379291a5c8b9e2a1ff66bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
